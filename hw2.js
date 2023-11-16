1ceac664e15c4931acff5f749ebe7eaee4a452f2
ToasteRu <voloshkonikita@bk.ru>
2023-11-16 16:39:19 +0300

    HomeWork_2

---
+++ hw2.js
@@ -0,0 +1,15 @@
+let secondString = "Сегодня хороший день.";
+let firstString = "96";
+let emptyString = "";
+let newNumber = 34;
+let falseName = false;
+
+let notSecondStr = Boolean (secondString);
+let notFirstStr = Number (firstString);
+let notEmptyStr = Boolean (emptyString);
+let notNewNumber = newNumber.toString();
+let notFalseName = String (falseName);
+
+console.log(notFirstStr);
+console.log(notEmptyStr);
+console.log(notNewNumber);
