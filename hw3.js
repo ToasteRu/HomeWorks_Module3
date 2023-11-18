967b3719d6af74ab47cc68fff49a470576696d15
ToasteRu <voloshkonikita@bk.ru>
2023-11-18 04:59:58 +0300

    HomeWork_3

---
+++ hw3.js
@@ -0,0 +1,14 @@
+let articlesArray = ["Почему я пошел на курс к Алексею","Чем я занимаюсь сейчас","Какой музыкальный фестиваль я хочу посетить"];
+
+articlesArray.unshift("Внедрение CRM системы PlanFix в бизнес");
+
+console.log("Порядковый номер статьи - ",articlesArray.indexOf("Чем я занимаюсь сейчас")+1);
+
+let popArticle1 = articlesArray.pop();
+console.log(popArticle1);
+let popArticle2 = articlesArray.pop();
+console.log(popArticle2);
+let popArticle3 = articlesArray.pop();
+console.log(popArticle3);
+let popArticle4 = articlesArray.pop();
+console.log(popArticle4);
