8bd9e41ae5065aa18c09b8ab31ba538141a690b5
ToasteRu <voloshkonikita@bk.ru>
2023-11-22 04:42:44 +0300

    HomeWork-5(att.2)

--- hw5.js
+++ hw5.js
@@ -5,9 +5,9 @@ function User (name){
 let newUser = new User("Nikita");
 console.log(newUser);
 //2. Функция Article
-function Article(ArtName,ArtTxt) {
-    this.name=ArtName;
-    this.text=ArtTxt;
+function Article(artName,artTxt) {
+    this.name=artName;
+    this.text=artTxt;
 }
 let newArticle = new Article("Чем я занимаюсь сейчас","Работаю в CRM-системе ПланФикс.")
 console.log(newArticle);
@@ -15,8 +15,8 @@ console.log(newArticle);
 let userObj = {};
 //4. Массив из 2 объектов статей
 let newArticle2 = new Article("Какой музыкальный фестиваль я хочу посетить","Tomorrowland, который проводится 4 раза в год в Бельгии");
-let Array = [newArticle,newArticle2];
-console.log(Array);
+let artArray = [newArticle,newArticle2];
+console.log(artArray);
 //5. Добавим поле articles (здесь храним массив статей) в объект
-userObj.articles=Array;
+userObj.articles=artArray;
 console.log(userObj);
